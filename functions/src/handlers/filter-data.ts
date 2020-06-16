e22317e4f75b3789e27a5a46dca6c12631c7c138
drys262 <koeichavez17@gmail.com>
2020-06-17 02:06:28 +0800

    chore: remove console.logs

--- functions/src/handlers/filter-data.ts
+++ functions/src/handlers/filter-data.ts
@@ -3,11 +3,8 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 export default async function filterData(req: functions.Request): Promise<any> {
-  console.log(req.query);
   const userId = req.query.userId as string;
   const filter = (req.query.filter as 'desc' | 'asc') || 'asc';
-  console.log('userId here 111');
-  console.log('filter here', filter);
   const db = admin.firestore();
   const data = await db
     .collection('people')
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,7 +25,6 @@ export const onDeleteUser = functions.auth
 export const httpFilterData = functions.https.onRequest(
   async (req: functions.Request, res: functions.Response) => {
     const data = await filterData(req);
-    console.log('data here 111', data);
     res.set('Access-Control-Allow-Origin', '*');
     res.status(200).send(data);
   }
